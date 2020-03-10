4fabc4c615b60b2bb04ee9eca89d82bab030c50e
MikeNikitin <mnikitin@plotpad.ru>
2020-03-10 14:19:12 +0300

    test commit

--- views/home/index.php
+++ views/home/index.php
@@ -8,7 +8,7 @@ View::setTitle('Welcome to My Application');
 
 <div class="embed-responsive embed-responsive-16by9">
     <div> 
-    hello world !
+    Hello, World!
     </div>
     <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/AvMFHHyhmVM?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe> -->
 </div>
